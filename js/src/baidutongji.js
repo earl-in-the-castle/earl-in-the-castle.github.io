06b0b50b568ee9c595e75218d79522c38f39c8d3
earl in the castle <390549721@qq.com>
2019-01-23 13:01:20 +0800

    Site updated: 2019-01-23 13:01:18

---
+++ js/src/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?affdeae734021e898f7eed184f3a1ae6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
